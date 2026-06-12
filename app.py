3a5fc664879a54d6cc2e1dfcf185b85a8d691e22
Suhani Srivastava <srivastavasuhani2005@gmail.com>
2026-06-12 21:24:34 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,118 @@
+from flask import Flask, render_template, Response, request, redirect, url_for, session
+from detector import generate_frames, detect_emotion_image
+import detector
+import base64
+import numpy as np
+import cv2
+from deepface import DeepFace
+app = Flask(__name__)
+app.secret_key = "emotion_ai_secret"
+
+# ---------------- LOGIN ----------------
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        if username == "admin" and password == "1234":
+            session['user'] = username
+            return redirect(url_for('home'))
+        else:
+            return render_template("login.html", error="Invalid Credentials")
+
+    return render_template("login.html")
+
+
+# ---------------- LOGOUT ----------------
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    return redirect(url_for('login'))
+
+
+# ---------------- HOME ----------------
+@app.route('/')
+def home():
+    if 'user' not in session:
+        return redirect(url_for('login'))
+    return render_template("index.html")
+
+# ---------------- VIDEO STREAM ----------------
+@app.route('/video')
+def video():
+    if 'user' not in session:
+        return redirect(url_for('login'))
+
+    return Response(generate_frames(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+# ---------------- IMAGE UPLOAD PAGE ----------------
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if 'user' not in session:
+        return redirect(url_for('login'))
+
+    emotion = None
+
+    if request.method == 'POST':
+        file = request.files['image']
+        path = "static/upload.jpg"
+        file.save(path)
+
+        emotion = detect_emotion_image(path)
+
+    return render_template("upload.html", emotion=emotion)
+
+# ---------------- LIVE EMOTION API ----------------
+@app.route('/live_emotion')
+def live_emotion():
+    return {"emotion": detector.latest_emotion}
+
+
+#-----------Analytics API----------------
+@app.route('/emotion_stats')
+def emotion_stats():
+   return detector.emotion_stats
+
+@app.route('/detect_frame', methods=['POST'])
+def detect_frame():
+    try:
+        data = request.json['image']
+
+        # Remove base64 header
+        image_data = data.split(',')[1]
+
+        image_bytes = base64.b64decode(image_data)
+
+        np_arr = np.frombuffer(image_bytes, np.uint8)
+
+        frame = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+
+        result = DeepFace.analyze(
+            frame,
+            actions=['emotion'],
+            enforce_detection=False
+        )
+
+        emotion = result[0]['dominant_emotion']
+        detector.latest_emotion = emotion
+
+        if emotion in detector.emotion_stats:
+         detector.emotion_stats[emotion] += 1
+
+
+        return {
+            "success": True,
+            "emotion": emotion
+        }
+
+    except Exception as e:
+        return {
+            "success": False,
+            "error": str(e)
+        }
+
+if __name__ == "__main__":
+    app.run(debug=True)
